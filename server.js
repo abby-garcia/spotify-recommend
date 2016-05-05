9278a54f32dd1e665a08177706ece6c7da75e2dd
abby-garcia <acg361@gmail.com>
2016-05-05 10:21:32 -0400

    server.js improvements

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var events = require('events');
 
 var getFromApi = function(endpoint, args) {
+    console.log('endpoint: '+endpoint);
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
            .qs(args)
@@ -16,6 +17,15 @@ var getFromApi = function(endpoint, args) {
             });
     return emitter;
 };
+
+var relatedArtist = function(artist,res) {
+    var relatedArtistReq = getFromApi('artists/' + artist.id + '/related-artists',{});
+    
+    relatedArtistReq.on('end', function(item) {
+        artist.related = item.artists;
+        res.json(artist);
+    });
+}
 
 
 // Express 
@@ -32,30 +42,13 @@ app.get('/search/:name', function(req, res) {
     });
 
     searchReq.on('end', function(item) {
-        var artistID = item.artists.items[0].id;
-        relatedArtist(artistID);
+        var artist = item.artists.items[0];
+        relatedArtist(artist,res);
     });
 
     searchReq.on('error', function(code) {
         res.sendStatus(code);
     });
-
-    function relatedArtist(id) {
-        var relatedArtistReq = getFromApi('artists/' + id + '/related-artists');
-
-        relatedArtistReq.on('end', function(item) {
-        var artists = item.artists;
-        res.json(artists);
-    });
-        relatedArtistReq.on('error', function(code) {
-        res.sendStatus(code);
-    });
-
-
-    }
-     
-
-
 
 });
 
